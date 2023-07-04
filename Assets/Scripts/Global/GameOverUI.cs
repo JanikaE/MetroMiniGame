45ca93e2b987a6dc58ca208cfc9b2126d7c0b1b7
Lan <qinzhihuan233@outlook.com>
2023-07-04 23:00:19 +0800

    合并

--- Assets/Scripts/Global/GameOverUI.cs
+++ Assets/Scripts/Global/GameOverUI.cs
@@ -21,18 +21,13 @@ public class GameOverUI : MonoBehaviour
         CurrentSceneName = currentSceneName;
     }
 
-    private void Awake()
-    {
-        Instance = this;
-    }
     /// <summary>
     /// 游戏结束时调用，显示游戏通关或游戏失败的面板
     /// </summary>
     /// <param name="sceneName">当前游戏场景的名字/param>
     /// <param name="isVictory">游戏是否通关</param>
-    public void GameOver(SceneName sceneName, bool isVictory)
+    public void GameOver(bool isVictory)
     {
-        CurrentSceneName = sceneName;
         if (isVictory)
         {
             gameVictoryPanel.gameObject.SetActive(true);
